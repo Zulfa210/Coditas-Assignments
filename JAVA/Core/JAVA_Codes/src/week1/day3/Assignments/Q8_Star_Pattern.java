aef624e552c20932be07af7129105aed06d0f99b
Zulfa-Attar-Coditas <zulfaattar@gmail.com>
2022-07-22 15:19:08 +0530

    Added Assignments for today

---
+++ JAVA/Core/JAVA_Codes/src/week1/day3/Assignments/Q8_Star_Pattern.java
@@ -0,0 +1,27 @@
+package week1.day3.Assignments;
+
+public class Q8_Star_Pattern {
+
+    public void print_Pattern(){
+
+
+        for (int i= 0; i<5 ; i++) //we have 5 rows to print
+        {
+            for (int j=0; j<9; j++) //the first row has maximum 9 columns
+            {
+                if(j>=i && j<=8-i)  //this will print stars from j =
+                System.out.print("*");
+                else
+                    System.out.print(" ");//for j = 0 and j= last index, space is printed
+            }
+            System.out.println("");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Q8_Star_Pattern star_pattern = new Q8_Star_Pattern();
+        star_pattern.print_Pattern();
+
+    }
+}
