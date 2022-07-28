422bb432eb4d180333488826bf0cffcb3d34fcfe
Zulfa-Attar-Coditas <zulfaattar@gmail.com>
2022-07-28 16:14:10 +0530

    Added Core Java Assignment for 28th July

--- JAVA/Core/JAVA_Codes/src/week2/day8/Assignments/Ques3_DriverClass.java
+++ JAVA/Core/JAVA_Codes/src/week2/day8/Assignments/Ques3_DriverClass.java
@@ -1,4 +1,12 @@
 package week2.day8.Assignments;
+//3). Create a class Medicine to represent a drug manufactured by a pharmaceutical company.
+// Provide a function display Label() in this class to print Name and address of the company.
+//Derive Tablet, Syrup and Ointment classes from the Medicine class.
+//Override the displayLabel() function in each of these classes to print additional
+// information suitable to the type of medicine.
+//For example, in case of tablets, it could be "store in a cool dry place",
+// in case of ointments it could be "for external use only" etc. Create a class TestMedicine. Write main function to do the following: Declare an array of Medicine references of size 10 Create a medicine object of the type as decided by a randomly generated integer in the range 1 to 3. Refer Java API Documentation to find out random generation features.
+// Check the polymorphic behavior of the displayLabel() method.
 
 import java.util.Random;
 import java.util.Scanner;
