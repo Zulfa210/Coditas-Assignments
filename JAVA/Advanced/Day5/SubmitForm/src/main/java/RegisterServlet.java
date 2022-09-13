f70579184ba01577fb877e59ef60b4cd768abe2f
Zulfa-Attar-Coditas <zulfaattar@gmail.com>
2022-09-13 20:33:02 +0530

    Added jsp code for 13th september-2nd video

--- JAVA/Advanced/Day5/SubmitForm/src/main/java/RegisterServlet.java
+++ JAVA/Advanced/Day5/SubmitForm/src/main/java/RegisterServlet.java
@@ -16,23 +16,27 @@ public class RegisterServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         resp.setContentType("text/html");
 
+        out.println("<h1>User Details</h1>");
         String name = req.getParameter("name");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
         String gender = req.getParameter("gender");
         String course = req.getParameter("course");
-        String condition = req.getParameter("condition");
+        String condition = "";
 
-        if(condition.equals("checked")){
-            out.println("<h2>Name: " + name + "</h2>");
-            out.println("<h2>Password: " + password + "</h2>");
-            out.println("<h2>Email: " + email + "</h2>");
-            out.println("<h2>Gender: " + gender + "</h2>");
-            out.println("<h2>Course: " + course + "</h2>");
-        }
-        else{
-            out.println("You have not Accepted the terms and conditions");
-        }
+        condition=   req.getParameter("condition");
 
+        if(condition!=null) {
+            if (condition.equals("checked")) {
+                out.println("<h2>Name: " + name + "</h2>");
+                out.println("<h2>Password: " + password + "</h2>");
+                out.println("<h2>Email: " + email + "</h2>");
+                out.println("<h2>Gender: " + gender + "</h2>");
+                out.println("<h2>Course: " + course + "</h2>");
+            }
+        }else{
+            out.println("<h2>You have not Accepted the terms and conditions<h2>");
+            out.println("<a href=\"index.html\"><button type =\"button\">Click here to Register Again</button></a>");
+        }
     }
 }
